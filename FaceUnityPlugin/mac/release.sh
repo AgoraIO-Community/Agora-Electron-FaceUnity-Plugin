87d2fa3886e9f0277049506e5e4c085b5d359bb9
张乾泽 <qianze.zhang@hotmail.com>
2019-10-12 18:20:10 +0800

    script update

--- FaceUnityPlugin/mac/release.sh
+++ FaceUnityPlugin/mac/release.sh
@@ -1,7 +1,9 @@
 wget https://github.com/Faceunity/FULiveDemoMac/releases/download/v6.2.0/FaceUnity-SDK-Mac-v6.2.0.zip -O FaceUnity.zip
 unzip -u FaceUnity.zip
 mkdir -p ../FULive
-mv FaceUnity-SDK-Mac FaceUnityPlugin/FULive/mac
+rm -rf ../FULive/mac
+mv FaceUnity-SDK-Mac ../FULive/mac
+rm -rf build
 xcodebuild -project FaceUnityPlugin.xcodeproj
 cd build
 mv ../../FULive/mac/Resources ./Release/assets
