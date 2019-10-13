7bf6497b6ba7c46823f5beb594ffa6203436fff1
Qianze Zhang <qianze.zhang@hotmail.com>
2019-10-13 16:01:18 +0800

    Update package.sh

--- FaceUnityPlugin-2.9.0/win/package.sh
+++ FaceUnityPlugin-2.9.0/win/package.sh
@@ -2,4 +2,4 @@ cp -rf ../FULive/win/assets ./Release/.
 curl -L -o face_beautification.bundle https://github.com/Faceunity/FULivePC/raw/v6.2-release/assets/face_beautification.bundle
 cp -rf ./face_beautification.bundle ./Release/assets/.
 rm -rf FaceUnityPlugin
-7z a -r -tzip fu-win.zip ./Release
+7z a -r -tzip fu-win-290.zip ./Release
