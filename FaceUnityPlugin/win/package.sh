fd283603a7eceda85e4e69d00fb77b6b55d3b178
your name <your email>
2019-10-13 14:36:04 +0800

    add face_beautification bundle in defualt win pack

--- FaceUnityPlugin/win/package.sh
+++ FaceUnityPlugin/win/package.sh
@@ -1,3 +1,5 @@
 cp -rf ../FULive/win/assets ./Release/.
+curl -L -o face_beautification.bundle https://github.com/Faceunity/FULivePC/raw/v6.2-release/assets/face_beautification.bundle
+cp -rf ./face_beautification.bundle ./Release/.
 rm -rf FaceUnityPlugin
 7z a -r -tzip fu-win.zip ./Release
