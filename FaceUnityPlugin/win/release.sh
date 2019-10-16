83b6b366c1fc836225cad27946e568de838be9e1
张乾泽 <qianze.zhang@hotmail.com>
2019-10-16 16:18:10 +0800

    script fix

--- FaceUnityPlugin/win/release.sh
+++ FaceUnityPlugin/win/release.sh
@@ -2,5 +2,4 @@ curl -L -o FaceUnityAll.zip https://github.com/Faceunity/FULivePC/releases/downl
 unzip -u FaceUnityAll.zip
 unzip -u LibUpdate_6.2.0_tf_phy_20190708165436_0.zip
 mkdir -p ../FULive/win
-rm -rf ../FULive/win
 mv LibUpdate_6.2.0_tf_phy_20190708165436_0 ../FULive/win
