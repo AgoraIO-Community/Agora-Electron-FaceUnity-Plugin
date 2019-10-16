481bb63f194e70aabd33c32409610be817cc34f2
张乾泽 <qianze.zhang@hotmail.com>
2019-10-16 15:41:24 +0800

    add to 290

--- FaceUnityPlugin-2.9.0/win/package.sh
+++ FaceUnityPlugin-2.9.0/win/package.sh
@@ -1,5 +1,6 @@
 cp -rf ../FULive/win/assets ./Release/.
 curl -L -o face_beautification.bundle https://github.com/Faceunity/FULivePC/raw/v6.2-release/assets/face_beautification.bundle
 cp -rf ./face_beautification.bundle ./Release/assets/.
+cp -rf ./FULive/win/Windows/win32_release/* ./Release/.
 rm -rf FaceUnityPlugin
-7z a -r -tzip fu-win-290.zip ./Release
+7z a -r -tzip fu-win.zip ./Release
