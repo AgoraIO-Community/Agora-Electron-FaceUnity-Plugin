8c623c723593a498f41bbcfb74300fbab046a3e6
张乾泽 <qianze.zhang@hotmail.com>
2019-10-25 15:58:27 +0800

    zm special version

--- FaceUnityPlugin/win/release.sh
+++ FaceUnityPlugin/win/release.sh
@@ -1,4 +1,4 @@
-curl -L -o FaceUnityAll.zip https://github.com/Faceunity/FULivePC/releases/download/v6.2-release/LibUpdate_6.2.0_ALL.docs._20190708165436_0.zip
+curl -L -o FaceUnityAll.zip https://github.com/AgoraIO-Community/Agora-Electron-FaceUnity-Plugin/releases/download/0.0.1/FULiveZM.zip
 unzip -u FaceUnityAll.zip
 unzip -u LibUpdate_6.2.0_tf_phy_20190708165436_0.zip
 mkdir -p ../FULive/win
