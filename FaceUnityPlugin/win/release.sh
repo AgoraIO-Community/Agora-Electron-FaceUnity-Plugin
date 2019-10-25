4189a009b84ba541fef0903f05efa5a2beb59aea
张乾泽 <qianze.zhang@hotmail.com>
2019-10-25 16:02:07 +0800

    script update

--- FaceUnityPlugin/win/release.sh
+++ FaceUnityPlugin/win/release.sh
@@ -1,5 +1,5 @@
 curl -L -o FaceUnityAll.zip https://github.com/AgoraIO-Community/Agora-Electron-FaceUnity-Plugin/releases/download/0.0.1/FULiveZM.zip
 unzip -u FaceUnityAll.zip
-unzip -u LibUpdate_6.2.0_tf_phy_20190708165436_0.zip
+# unzip -u LibUpdate_6.2.0_tf_phy_20190708165436_0.zip
 mkdir -p ../FULive/win
-mv LibUpdate_6.2.0_tf_phy_20190708165436_0/* ../FULive/win/.
+mv Windows/* ../FULive/win/.
