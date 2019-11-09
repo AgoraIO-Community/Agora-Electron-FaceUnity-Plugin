5beb5a1d1ac50e3368bb7a4f77840c76fb2450dd
your name <your email>
2019-11-09 21:03:14 +0800

    zm 1109 version

--- FaceUnityPlugin/win/release.sh
+++ FaceUnityPlugin/win/release.sh
@@ -1,4 +1,4 @@
-curl -L -o FaceUnityAll.zip https://github.com/AgoraIO-Community/Agora-Electron-FaceUnity-Plugin/releases/download/0.0.1/FULiveZM.zip
+curl -L -o FaceUnityAll.zip https://github.com/AgoraIO-Community/Agora-Electron-FaceUnity-Plugin/releases/download/0.0.1/FULiveZM1109.zip
 unzip -u FaceUnityAll.zip
 # unzip -u LibUpdate_6.2.0_tf_phy_20190708165436_0.zip
 mkdir -p ../FULive/win
